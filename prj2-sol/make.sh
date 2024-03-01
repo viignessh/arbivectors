df361643db2f875376e469ba5fb44088a23ca025
Vignesh Prabhakar <39653916+viignessh@users.noreply.github.com>
2024-03-01 03:26:22 -0500

    Delete prj2-sol directory

--- prj2-sol/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-path=`realpath $0`
-dir=`dirname $path`
-# $dir now refers to the directory in which this script actually lives
-gcc $dir/prj2-sol.c -o run
--- prj2-sol/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-path=`realpath $0`
-dir=`dirname $path`
-# $dir now refers to the directory in which this script actually lives
-./run $1 $2
